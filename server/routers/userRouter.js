ff56da290dfcb23dc372e24343f17853bd637842
Shubh Nawani <shubhnawani@outlook.com>
2025-03-23 20:31:17 +0530

    put api implemented

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {getUsers, addUser} = require('../controllers/userController')
+const {getUsers, addUser, updateUser} = require('../controllers/userController')
 const router = express.Router()
 
 router.get("/users", getUsers)
 router.post("/users", addUser)
+router.put("/users/:email", updateUser)
 
 module.exports = router
